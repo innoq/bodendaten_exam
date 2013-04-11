c018cb764a9469736273d45c0425e91015cc1582
Robert Glaser <mail@robert-glaser.de>
2013-04-11 16:23:55 +0200

    don't connect to db while precompiling assets on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,5 +4,7 @@ if Iqvoc::BodendatenExam.const_defined?(:Application)
   Iqvoc::BodendatenExam::Application.configure do
     # Settings specified here will take precedence over those in config/environment.rb
     Iqvoc::Environments.setup_production(config)
+
+    config.assets.initialize_on_precompile = false
   end
 end
